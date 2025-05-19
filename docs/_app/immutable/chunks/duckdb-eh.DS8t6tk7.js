8f7e33eda0abad10ce957ca1254d6f920428f011
Paul Wright <pwright@redhat.com>
2025-05-19 14:11:42 +0100

    update

---
+++ docs/_app/immutable/chunks/duckdb-eh.DS8t6tk7.js
@@ -0,0 +1,1 @@
+const a="/apicurio-docs-analysis/_app/immutable/assets/duckdb-eh.DrTJ_0hP.wasm";export{a as default};
---
+++ docs/_app/immutable/entry/start.Ca5eTqDM.js
@@ -0,0 +1,1 @@
+import{f as a}from"../chunks/entry.C3VnqoIs.js";export{a as start};
